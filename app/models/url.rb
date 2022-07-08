341b05a3ae94167ae13c4e791bc6d2e3ef8a7297
Drodmarc <rodmarc1207@gmail.com>
2022-07-08 14:28:36 +0800

    add link shortened feature

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,4 +4,13 @@ class Url < ApplicationRecord
   validates_format_of :long_url, :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
   belongs_to :user
 
+  before_create :generate_short_url
+
+  def generate_short_url
+    loop do
+      @short = SecureRandom.base64(6)
+      break if not Url.exists?(short_url: @short)
+    end
+    self.short_url=@short
+  end
 end
