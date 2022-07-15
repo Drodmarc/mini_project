42d6b8db9766ea2c169356dade61cb53f5004762
Drodmarc <rodmarc1207@gmail.com>
2022-07-15 16:06:32 +0800

    Remove other keys

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -3,7 +3,7 @@ class Api::RegionsController < ApplicationController
     @regions = Region.all
     respond_to do |format|
       format.html
-      format.json { render json: @regions.to_json(except: [:id])}
+      format.json { render json: @regions.to_json(except: [:id, :created_at, :updated_at])}
     end
   end
 end
