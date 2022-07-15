36b1310434d8fcb8ff8dff68fbeedc01e00d01f9
Drodmarc <rodmarc1207@gmail.com>
2022-07-15 12:59:45 +0800

    Migration for region model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_085529) do
+ActiveRecord::Schema.define(version: 2022_07_15_045322) do
+
+  create_table "regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "region_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "urls", charset: "utf8mb4", force: :cascade do |t|
     t.string "description"
